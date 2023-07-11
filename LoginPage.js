857104416dab25d2520b6bf67465c9654624e7ce
m.tanzeel.ext@tahaluf.ae <m.tanzeel.ext@tahaluf.ae>
2023-07-11 12:45:29 +0400

    Login changes

---
+++ LoginPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+function LoginPage(props) {
+  return <View style={styles.container}>Login</View>;
+}
+
+export default LoginPage;
---
+++ components/Homescreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button, View, Text } from "react-native";
+import SizedBox from "./SizedBox";
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen1</Text>
+      <SizedBox height={16} />
+
+      <Button
+        title="Go to About"
+        onPress={() => navigation.navigate("About")}
+      />
+      <SizedBox height={16} />
+
+      <Button
+        title="Go to Login"
+        onPress={() => navigation.navigate("Login")}
+      />
+    </View>
+  );
+}
